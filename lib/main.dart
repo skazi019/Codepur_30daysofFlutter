c0b0ecaf0e2273bf749ee2edae2c222742aac911
Kaushal Sharma <skazi019@gmail.com>
2022-03-10 20:27:31 +0530

    day3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Material(
-        child:
-          Center(
-            child: Container(
-              child: Text("Welcome to 30 days of flutter"),      
-            )
-          )
-      )
+      // home: const HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": ((context) => const LoginPage()),
+        "/home": ((context) => const HomePage()),
+        "/login": ((context) => const LoginPage())
+      },
     );
   }
 }
