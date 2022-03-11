69503f87246555c0ac106820c88b361526c15163
Kaushal Sharma <skazi019@gmail.com>
2022-03-11 09:18:57 +0530

    TextFormField and Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: const HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         // brightness: Brightness.dark,
         primarySwatch: Colors.green,
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/": ((context) => const LoginPage()),
         "/home": ((context) => const HomePage()),
